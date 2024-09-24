f33541d55202a7eb84b27fda0a16cf24e721f211
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2024-09-24 19:15:35 +0300

    finish lesson 1

---
+++ lesson-1/project-backend/src/index.js
@@ -0,0 +1,21 @@
+import * as fs from "node:fs/promises";
+import DetectFileEncodingAndLanguage from "detect-file-encoding-and-language";
+
+const func = async()=> {
+    // const buffer = await fs.readFile("./src/text.txt");
+    // const text = buffer.toString();
+    // console.log(text);
+    // const text = await fs.readFile("./src/text.txt", "utf-8");
+    // console.log(text);
+    // const {encoding} = await DetectFileEncodingAndLanguage("./src/text.txt");
+    // const text = await fs.readFile("./src/text.txt", encoding);
+    // console.log(text);
+    // await fs.appendFile("./src/text.txt", "\nPHP forever");
+    // await fs.writeFile("./src/text.txt", "Mojo the best");
+    // await fs.appendFile("./src/text2.txt", "\nPHP forever");
+    // await fs.writeFile("./src/text3.txt", "Mojo the best");
+    // await fs.unlink("./src/text3.txt");
+};
+
+
+func();
